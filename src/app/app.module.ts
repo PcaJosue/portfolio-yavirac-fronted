bb6c1e5d9606d0d0c034cabae8afaecbcba683a9
Josselyn Mena <josselynmena@MacBook-Pro-de-Josselyn.local>
2024-07-02 10:53:08 -0500

    agregado componentes-cat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatTableModule} from '@angular/material/table';
+import { CatalogosComponent } from './componentes/catalogos/catalogos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatalogosComponent
   ],
   imports: [
+    MatTableModule,
     BrowserModule,
     AppRoutingModule
   ],
