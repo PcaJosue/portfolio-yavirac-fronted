f3f50f16a61f6af96d3fff38c1c84181c390a82b
Leonardo Paul Carrillo Arce <leonardo@MacBook-Pro-de-Leonardo.local>
2024-07-16 15:21:43 -0500

    Enviroments Config

--- src/app/componentes/service.service.spec.ts
+++ src/app/componentes/service.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ServiceService } from './service.service';
+import { PeriodicElementService } from './service.service';
 
 describe('ServiceService', () => {
-  let service: ServiceService;
+  let service: PeriodicElementService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ServiceService);
+    service = TestBed.inject(PeriodicElementService);
   });
 
   it('should be created', () => {
